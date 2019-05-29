6759f16a463814e6e2dc9e60024ee7d68c70cdf6
Patryk <blyszczpatryk@gmail.com>
2019-05-29 13:48:36 +0200

    Add files, modify files

--- js/script.js
+++ js/script.js
@@ -33,3 +33,18 @@ flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
+
+
+window.initMap = function() {
+
+  	var uluru = {lat: -25.363, lng: 131.044};
+
+  	var map = new google.maps.Map(
+    	document.getElementById('map'), {zoom: 4, center: uluru});
+
+
+  	items.forEach(function(carousel) {
+  		new google.maps.Marker({position: carousel.coords, map: map})
+  	});
+	
+}
